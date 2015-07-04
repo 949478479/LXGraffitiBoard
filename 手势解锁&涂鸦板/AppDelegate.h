fa325524aa24af7d2d87f5dc3787c81f9b599be6
949478479 <949478479@qq.com>
2015-07-04 08:51:59 +0800

    Initial Commit

---
+++ 手势解锁&涂鸦板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手势解锁&涂鸦板
+//
+//  Created by 从今以后 on 15/7/4.
+//  Copyright (c) 2015年 949478479. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手势解锁&涂鸦板/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  手势解锁&涂鸦板
+//
+//  Created by 从今以后 on 15/7/4.
+//  Copyright (c) 2015年 949478479. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
